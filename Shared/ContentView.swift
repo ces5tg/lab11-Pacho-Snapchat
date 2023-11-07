4ebefb7f231149c7ef2fe985705a26f5e3278b3e
cesar <cesar.pacho@tecsup.edu.pe>
2023-11-07 17:00:52 -0500

    primero

---
+++ Shared/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Shared
+//
+//  Created by cesar pacho on 7/11/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Shared/lab11_snapchat_ios2App.swift
@@ -0,0 +1,17 @@
+//
+//  lab11_snapchat_ios2App.swift
+//  Shared
+//
+//  Created by cesar pacho on 7/11/23.
+//
+
+import SwiftUI
+
+@main
+struct lab11_snapchat_ios2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
